8743d9f9357214674ac60219a54160b2e340d7af
amrwb <yibyuk@icloud.com>
2019-03-02 16:13:44 +0900

    Change defconfig to j5lte_kor

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 export PATH=$PATH:$HOME/arm-eabi-4.8/bin/
 mkdir  out
-make -C $(pwd) O=$(pwd)/out ARCH=arm CROSS_COMPILE=arm-eabi- VARIANT_DEFCONFIG=msm8916_sec_j5xlte_skt_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=$(pwd)/out ARCH=arm CROSS_COMPILE=arm-eabi- VARIANT_DEFCONFIG=msm8916_sec_j5lte_kor_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 
 make -C $(pwd) O=$(pwd)/out ARCH=arm CROSS_COMPILE=arm-eabi-
 
