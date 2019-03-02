c2679f07dae1a019009ffce39f2cd4c7581973ec
amrwb <yibyuk@icloud.com>
2019-03-02 15:44:38 +0900

    Set Toolchain Location

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin:$PATH
+export PATH=$PATH:$HOME/arm-eabi-4.8/bin/
 mkdir  out
 make -C $(pwd) O=$(pwd)/out ARCH=arm CROSS_COMPILE=arm-eabi- VARIANT_DEFCONFIG=msm8916_sec_j5xlte_skt_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 
